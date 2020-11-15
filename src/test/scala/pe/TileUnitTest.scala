62753b8b2e9fbc9762cf656b8289eee567509148
Mason Ma <jiaaom@uci.edu>
2020-11-15 05:45:55 -0800

    Support n x n systolic array

--- src/test/scala/pe/TileUnitTest.scala
+++ src/test/scala/pe/TileUnitTest.scala
@@ -1,17 +1,19 @@
 package pe
 
+import scala.collection.mutable.ListBuffer
+import scala.math.pow
 import chisel3._
 import chisel3.util._
 import chisel3.iotesters
 import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
 
 
-class TileModuleTester(c: Tile) extends PeekPokeTester(c) {
+class TileModuleTester(c: Tile, input_arr: Array[Array[Int]], weight_arr: Array[Int]) extends PeekPokeTester(c) {
     def printVec[T <: BigInt](v: Seq[T]) = {
         for(i <- 0 until v.length){
-            print(v(i).toInt + " ")
+            print(" " + v(i).toInt)
         }
-        println("")
+        print("\n")
     }
 
     def pokeVec[T <: Bits](v: Vec[T], s: Seq[T]) = {
@@ -20,32 +22,68 @@ class TileModuleTester(c: Tile) extends PeekPokeTester(c) {
         }
     }
 
-    poke(c.io.weightEnable, true.B)
-    poke(c.io.typeSelection, 0.U)
-    pokeVec(c.io.weightsFlattened, Seq(5.U, 2.U, 1.U, 2.U))
+    def generateInputs(cycle: Int, input_arr: Array[Array[Int]]):Seq[UInt] = {
+        val cycleOffset = 0
+        val m = input_arr.size
+        val n = input_arr(0).size
+        var results = new ListBuffer[UInt]()
 
-    private var inputs = Seq(4.U, 0.U)
-    for (i <- 0 until inputs.size) {
-        poke(c.io.inputs(i), inputs(i))
+        for(j <- 0 until input_arr(0).size){
+            var currentRow = cycle + cycleOffset - j
+            //println("Current Row: " + currentRow)
+            if (currentRow < 0 || currentRow >= m)
+                results += 0.U
+            else
+                results += input_arr(currentRow)(j).U
+        }
+        val result = results.toSeq
+        /*for debugging purpose
+        println("Generated inputs:")
+        for(element<-result){  
+            print(element.toInt+" ")
+        }
+        */
+        return result
     }
 
-    step(1)
-    printVec(peek(c.io.outputs))
-    pokeVec(c.io.inputs, Seq(3.U, 2.U))
+    def mapToUint(arr: Array[Int]): Seq[UInt] = {
+        val s: Seq[UInt] = arr.map(x => x.U)
+        return s
+    }
 
-    step(1)
-    printVec(peek(c.io.outputs))
-    pokeVec(c.io.inputs, Seq(0.U, 1.U))
+    val rows = input_arr.size
+    val cols = weight_arr.size / input_arr.size
+    var resultMatrix = Array.ofDim[Int](rows, cols)
 
-    step(1)
-    printVec(peek(c.io.outputs))
-    pokeVec(c.io.inputs, Seq(0.U, 0.U))
-    step(1)
+    poke(c.io.weightEnable, true.B)
+    poke(c.io.typeSelection, 0.U)
+    pokeVec(c.io.weightsFlattened, mapToUint(weight_arr))
+    var cycleCount = 0
 
-    printVec(peek(c.io.outputs))
+    for(i <- 0 until 3*rows-2){
+        pokeVec(c.io.inputs, generateInputs(cycleCount, input_arr))
+        cycleCount += 1
+
+        step(1)
+        print("Cycle Count "+ cycleCount + ": ")
+        printVec(peek(c.io.outputs))
+
+        for(j <- 0 until cols){
+            if(rows+j<=cycleCount && cycleCount<rows*2+j)
+                resultMatrix(cycleCount-rows-j)(j) = peek(c.io.outputs)(j).toInt
+        }
+    }
+
+    println("=== Result matrix ===")
+    resultMatrix.foreach(row => {
+        row.foreach(e => print(f"$e%4d "))
+        print("\n")
+        }
+    )
 }
 
 // Executor
+/*
 class TileFlatSpec extends ChiselFlatSpec {
     chisel3.iotesters.Driver.execute(
         Array("--backend-name", "verilator", "--is-verbose"),
@@ -53,9 +91,4 @@ class TileFlatSpec extends ChiselFlatSpec {
     ) { c =>new TileModuleTester(c)
     } should be(true)
 }
-
-object TileMain extends App {
-    iotesters.Driver.execute(args, () => new Tile(32, 2, 2, true)) {
-        c => new TileModuleTester(c)
-    }
-}
+*/
